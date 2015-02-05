3b8bde5a01d3d359243e9d49333a005b4837f0d9
Lkytal <lkytal@live.cn>
2015-02-05 14:24:27 +0800

    3.0.0

--- meta/popsearch.meta.js
+++ meta/popsearch.meta.js
@@ -2,12 +2,12 @@
 // @name					Popup Search
 // @author					lkytal
 // @namespace				Lkytal
-// @homepage				http://coldfire.qiniudn.com/
+// @homepage				http://lkytal.github.io/
 // @description				Popup Search Box and translate Button (etc) for selected Text
 // @include					*
 // @exclude					*/test/index.html*
 // @require					http://libs.baidu.com/jquery/2.1.1/jquery.min.js
-// @version					2.9.8
+// @version					3.0.0
 // @icon					http://lkytal.qiniudn.com/ic.ico
 // @grant					GM_xmlhttpRequest
 // @grant					GM_addStyle
