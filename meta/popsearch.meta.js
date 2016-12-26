39359b363bb2c5c51f9139324fd509379ba9d09b
Lkytal <lkytal@live.cn>
2016-12-27 01:40:41 +0800

    4.0.0

--- meta/popsearch.meta.js
+++ meta/popsearch.meta.js
@@ -9,7 +9,7 @@
 // @exclude					http://acid3.acidtests.org/*
 // @exclude					http://www.acfun.tv/*
 // @require					https://cdn.bootcss.com/jquery/3.1.1/jquery.min.js
-// @version					3.5.0
+// @version					4.0.0
 // @icon					http://lkytal.qiniudn.com/ic.ico
 // @grant					GM_xmlhttpRequest
 // @grant					GM_addStyle
